2c26d698b5a7f5597d1a521b4edfd5e7f9bd6e5d
ArianKG <72304422+ArianKG@users.noreply.github.com>
2021-01-08 16:45:33 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('.btn');
+btn.onclick = randomUnicode;
+const txt = document.querySelector('.textarea');
+txt.spellcheck = false;
+
+function randomUnicode() {
+    document.body.innerHTML = '';
+    var unicode;
+    if (JSON.parse(txt.value).isInterval == 'true') {
+        setInterval(
+        function randomUnicode() {
+            unicode = String.fromCharCode(Math.floor(Math.random() * 65535));
+            document.body.innerHTML = unicode;
+        }, 1000)
+    }
+    unicode = String.fromCharCode(Math.floor(Math.random() * 65535));
+    document.body.innerHTML = unicode;
+}
